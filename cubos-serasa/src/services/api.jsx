60e9a874f7cada894c3d85850b63a02f8917df6f
Matheus Piovezan <matheuspiovezan@outlook.com>
2023-09-04 13:44:48 -0300

    updating

--- cubos-serasa/src/services/api.jsx
+++ cubos-serasa/src/services/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://backcubos-1-n6836554.deta.app",
+  baseURL: "https://backserasa-1-a0644002.deta.app",
   headers: { "Content-Type": "application/json" },
 });
